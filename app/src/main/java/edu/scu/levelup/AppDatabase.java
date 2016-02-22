b8fbb31cdc4b6d09a2a4dc3dcde836712e3d412c
Ajay Videkar <john@Throbbing-Brains.local>
2016-02-21 21:06:33 -0800

    Fixing database bugs

--- app/src/main/java/edu/scu/levelup/AppDatabase.java
+++ app/src/main/java/edu/scu/levelup/AppDatabase.java
@@ -1,8 +1,10 @@
 package edu.scu.levelup;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by ajay on 2016-02-20.
@@ -19,7 +21,7 @@ public class AppDatabase extends SQLiteOpenHelper {
     public static final String COLUMN_EMAILID = "emailid";
     public static final String COLUMN_PASSWORD = "password";
     public static final String query = "CREATE TABLE "+TABLE_NAME+"("+COLUMN_ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "+COLUMN_ROLE+" VARCHAR(255), "+COLUMN_FULLNAME+" VARCHAR(255), "+COLUMN_AGE+" VARCHAR(5), "+COLUMN_PHONE_NUMBER+" VARCHAR(255), "+COLUMN_EMAILID+" VARCHAR(320), "+COLUMN_PASSWORD+" VARCHAR(255));";
-
+    SQLiteDatabase db;
 
     AppDatabase(Context context)
     {
@@ -28,6 +30,32 @@ public class AppDatabase extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(query);
+    }
+
+    public String Login(String username)
+    {
+        db = this.getReadableDatabase();
+        String query = "Select emailid, password from "+TABLE_NAME;
+        Cursor cursor = db.rawQuery(query, null);
+        String userName;
+        String passWord;
+        passWord = "Not Found";
+        Log.e("AJAY", " VIDEKAR " + passWord);
+        if(cursor.moveToFirst())
+        {
+            do {
+                userName = cursor.getString(0);
+                Log.e("AJAY", " VIDEKAR " + userName);
+                if (userName == username)
+                {
+                    passWord = cursor.getString(1);
+                    break;
+                }
+            }
+            while (cursor.moveToNext());
+        }
+        return passWord;
+
     }
 
     @Override
