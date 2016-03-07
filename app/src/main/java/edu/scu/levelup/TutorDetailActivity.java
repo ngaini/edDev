3de3811e48764686e2cf9d3a956fa29255a67666
Nathan Gaini <nathan.gaini3@gmail.com>
2016-03-07 00:19:22 -0800

    sync local with remote

--- app/src/main/java/edu/scu/levelup/TutorDetailActivity.java
+++ app/src/main/java/edu/scu/levelup/TutorDetailActivity.java
@@ -1,5 +1,6 @@
 package edu.scu.levelup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.TextView;
@@ -29,6 +30,10 @@ public class TutorDetailActivity extends Activity {
         String name = extra.getString("name");
         final TextView tutorName_id = (TextView)this.findViewById(R.id.TDA_tutorName_textView);
         final TextView tutorExpertize_id = (TextView)this.findViewById(R.id.TDA_tutorExpertise_textView);
+        final TextView tutorAge_id = (TextView)this.findViewById(R.id.TDA_tutorAge_textView);
+        final TextView tutorDescription_id = (TextView)this.findViewById(R.id.TDA_tutorDescription_textView);
+        final TextView tutorGender_id = (TextView)this.findViewById(R.id.TDA_tutorGender_textView);
+        final TextView tutorEducation_id = (TextView)this.findViewById(R.id.TDA_tutorEducation_textView);
         //Setting name to textView
 //        ((TextView)this.findViewById(R.id.TDA_tutorName_textView)).setText(name);
 
@@ -39,15 +44,24 @@ public class TutorDetailActivity extends Activity {
             queryRef.addChildEventListener(new ChildEventListener() {
                 @Override
                 public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                    Map<String,Object> map = (Map<String, Object>) dataSnapshot.getValue();
-                    String name =(String) map.get("fullName");
-                    String expertise = (String)map.get("interests");
-                    String description = (String)map.get("description");
-                    String gender = (String)map.get("gender");
-                    long age = (long)map.get("age");
-                    Toast.makeText(TutorDetailActivity.this, ""+name+"::"+expertise+"::"+gender+"::"+age+"::"+description,Toast.LENGTH_LONG).show();
-                    tutorName_id.setText(name);
-                    tutorExpertize_id.setText(expertise);
+//                    Map<String,Object> map = (Map<String, Object>) dataSnapshot.getValue();
+//                    String name =(String) map.get("fullName");
+//                    String expertise = (String)map.get("interests");
+//                    String description = (String)map.get("description");
+//                    String gender = (String)map.get("gender");
+//                    long age = (long)map.get("age");
+//                    Toast.makeText(TutorDetailActivity.this, ""+name+"::"+expertise+"::"+gender+"::"+age+"::"+description,Toast.LENGTH_LONG).show();
+                    Users user = dataSnapshot.getValue(Users.class);
+
+
+//                    tutorName_id.setText(name);
+//                    tutorExpertize_id.setText(expertise);
+                    tutorName_id.setText(user.getFullName());
+                    tutorExpertize_id.setText("Gender: "+user.getInterests());
+                    tutorAge_id.setText(user.getAge()+" yrs");
+                    tutorEducation_id.setText("Highest Education:\n"+user.getEducation());
+                    tutorGender_id.setText("Gender: "+user.getGender());
+                    tutorDescription_id.setText("Description:\n"+user.getDescription());
                 }
 
                 @Override
@@ -70,6 +84,14 @@ public class TutorDetailActivity extends Activity {
 
                 }
             });
+        }
+
+        //incase the tutor happens to delete his account
+        else if(name.isEmpty())
+        {
+
+            Intent tutorUnAvailableIntent = new Intent(TutorDetailActivity.this, StudentsListActivity.class);
+            startActivity(tutorUnAvailableIntent);
         }
 
     }
